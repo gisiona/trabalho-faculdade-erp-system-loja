ecbf9b37d1f33830ac565f7d49ab004434f355b6
Gisiona <gil-real@hotmail.com>
2014-11-22 19:51:14 -0200

    ERPSYSTEM - Adicionado a classe Clientes no projeto Dominio.

---
+++ ErpSystem.Dominio/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpSystem.Dominio
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
