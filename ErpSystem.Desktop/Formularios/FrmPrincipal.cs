74c8e2e0c79508c8f3b08f59dba6110254529137
acslook <acslook@hotmail.com>
2014-11-26 10:35:34 -0200

    ERPSYSTEM - Comitt Adriano

---
+++ ErpSystem.Desktop/Formularios/FrmPrincipal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ErpSystem.Desktop
+{
+    public partial class FrmPrincipal : ErpSystem.Desktop.FrmClientes
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            FrmClientes frm = new FrmClientes();
+            frm.ShowDialog();
+        }
+    }
+}
