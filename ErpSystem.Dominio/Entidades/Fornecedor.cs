0985345811d86521542cd87b2342e0172f65b901
Gisiona <gil-real@hotmail.com>
2014-11-26 15:32:50 -0200

    ERPSYSTEM - Adicionado a classe Fornecedor no projeto Dominio

---
+++ ErpSystem.Dominio/Entidades/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpSystem.Dominio.Entidades
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
